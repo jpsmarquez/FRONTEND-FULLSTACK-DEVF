e5a3b92a5d322deac9d5e4731bac66d4108fb7ca
JP'S <delorean@DeLoreans-MacBook-Pro.local>
2020-03-22 18:31:50 -0600

    DIFFERENT TITLE

--- src/views/Home.js
+++ src/views/Home.js
@@ -27,7 +27,7 @@ function Home() {
 
   return (
      
-       <Layout title="APP" >
+       <Layout title="APPLE" >
            {data.getEvents.map(event => (
                 <Card
                   id={event._id}
