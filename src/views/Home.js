eb60774901581c6596c94be372804100d6832a16
JP'S <delorean@DeLoreans-MacBook-Pro.local>
2020-03-22 18:47:19 -0600

    web app

--- src/views/Home.js
+++ src/views/Home.js
@@ -27,7 +27,7 @@ function Home() {
 
   return (
      
-       <Layout title="TEMPLATE APP" >
+       <Layout title="WEB APP" >
            {data.getEvents.map(event => (
                 <Card
                   id={event._id}
