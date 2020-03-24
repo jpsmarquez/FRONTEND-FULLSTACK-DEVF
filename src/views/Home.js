f97e60298eeac6a7d404945fe1046649b7ebdc66
JP'S <delorean@DeLoreans-MacBook-Pro.local>
2020-03-24 00:18:41 -0600

    flow

--- src/views/Home.js
+++ src/views/Home.js
@@ -27,7 +27,7 @@ function Home() {
 
   return (
      
-       <Layout title="WEB APP" >
+       <Layout title="flow" >
            {data.getEvents.map(event => (
                 <Card
                   id={event._id}
