2216fbff953d08744d6880fd24071b2ad97350a9
JP'S <delorean@DeLoreans-MacBook-Pro.local>
2020-04-20 16:31:42 -0500

    template

--- src/views/Home.js
+++ src/views/Home.js
@@ -27,7 +27,7 @@ function Home() {
 
   return (
      
-       <Layout title="CIRQLOS" >
+       <Layout title="TEMPLATE" >
            {data.getEvents.map(event => (
                 <Card
                   id={event._id}
